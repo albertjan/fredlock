34fa333988e998dd3548e2022faf59ca22c2bbf8
Albert-Jan Nijburg <anijburg@autotask.com>
2016-11-22 20:33:56 +0000

    working towards the first test

--- src/fredlock/fredlock.fs
+++ src/fredlock/fredlock.fs
@@ -5,7 +5,7 @@ open System;
 
 type Lock = {
   resource: string;
-  value: Option<string>;
+  value: string option;
   expiration: int64;
 }
 
@@ -16,23 +16,54 @@ type Lock = {
 ///     let h = Library.hello 1
 ///     printfn "%d" h
 ///
-type Redlock (servers: list<string>) =
+type Redlock (mp: ConnectionMultiplexer, ?db: int) =
+  
+  let lockScript = @"return redis.call(""set"", KEYS[1], ARGV[1], ""NX"", ""PX"", ARGV[2])" 
+  let unlockScript = 
+    @"if redis.call(""get"", KEYS[1]) == ARGV[1] then return redis.call(""del"", KEYS[1]) else return 0 end" 
+  let extendScript = 
+    @"if redis.call(""get"", KEYS[1]) == ARGV[1] then return redis.call(""pexpire"", KEYS[1], ARGV[2]) else return 0 end"
+
+  let dbFactory () =
+    match db with
+    | Some i -> mp.GetDatabase(i)
+    | _ -> mp.GetDatabase()
+
   let epoch = new DateTime(1970, 1, 1)
 
   let toEpoch date =
     let diff:TimeSpan = (epoch - date)
     diff.TotalMilliseconds |> int64
 
-  let internalLock resource value (ttl:int64) =
+  let random () = 
+    let arr = Array.create 16 0uy
+    let rand:Random = new Random(Guid.NewGuid().GetHashCode())
+    rand.NextBytes(arr)
+    arr 
+      |> Array.map (fun s -> s.ToString("x2"))
+      |> String.concat "" 
+
+  let internalLock (resource:string) (value:string option) (ttl:int64) =
     async {
+
+      let db = dbFactory()
+
+      let! value =  
+        match value with 
+        | Some r -> async { return RedisResult.Create(RedisValue.op_Implicit(r)) }
+          //extend
+        | None ->
+          //lock
+          let key = RedisKey.op_Implicit(resource)
+          let value = RedisValue.op_Implicit(random())
+          db.ScriptEvaluateAsync(lockScript, [|key|], [|value|]) |> Async.AwaitTask
+
       return {
         resource = resource;
-        value = value;
+        value = value |> string |> Some;
         expiration = DateTime.Now.AddMilliseconds(ttl |> float) |> toEpoch;
       }
     }
-
-  let mp = ConnectionMultiplexer.Connect(servers |> String.concat ",")
 
   /// Returns 42
   ///
@@ -53,10 +84,19 @@ type Redlock (servers: list<string>) =
   member this.Release lock =
     async {
       let lock = { lock with expiration = 0L }
-      let votes = 0
-      let quorum = (Math.Floor(servers.Length / 2 |> float) |> int) + 1;
-      let a:IServer = mp.GetServer("a")
-      let b:IDatabase = mp.GetDatabase()
-      
-      ()    
+      let db = dbFactory()
+      do! 
+        match lock.value with
+        | Some l -> 
+          async { 
+            let! res =
+              db.ScriptEvaluateAsync(unlockScript, [|lock.resource |> RedisKey.op_Implicit|], [| l |>  RedisValue.op_Implicit|])
+              |> Async.AwaitTask
+              
+            let i:string = RedisResult.op_Explicit(res)
+            () //return (i |> int64)
+          }
+        | None ->
+          async { () }
+      ()
     }
--- tests/fredlock.Tests/Tests.fs
+++ tests/fredlock.Tests/Tests.fs
@@ -2,9 +2,14 @@ module fredlock.Tests
 
 open fredlock
 open NUnit.Framework
+open StackExchange.Redis
 
 [<Test>]
-let ``hello returns 42`` () =
-  //let result = Library.hello 42
-  //printfn "%i" result
-  Assert.AreEqual(42,42)
+let ``should write lock on all servers`` () =
+  async {
+    let mp = ConnectionMultiplexer.Connect("127.0.0.1")
+    let rl = Redlock(mp)
+    let! res = rl.Lock "bananas" 100L
+    Assert.AreEqual(res.resource, "bananas")
+  } |> Async.RunSynchronously |> ignore
+
